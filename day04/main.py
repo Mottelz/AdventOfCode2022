64d6614f93139eadf1e7b66a5c322bc51f04a683
Mottelz <shachna@gmail.com>
2022-12-04 00:36:22 -0500

    Day 4 completed

---
+++ day04/main.py
@@ -0,0 +1,45 @@
+from utils.readers import read_file_and_split_lines
+from typing import *
+
+
+def do_ranges_overlap_completely(r1: str, r2: str) -> bool:
+    range1 = create_range(r1)
+    range2 = create_range(r2)
+    intersection = set(range2).intersection(range1)
+    return len(intersection) == len(range2) or len(intersection) == len(range1)
+
+
+def do_ranges_overlap(r1: str, r2: str) -> bool:
+    range1 = create_range(r1)
+    range2 = create_range(r2)
+    intersection = set(range2).intersection(range1)
+    return len(intersection) > 0
+
+
+def create_range(raw_range: str) -> Iterable:
+    split = raw_range.split('-')
+    return range(eval(split[0]), eval(split[1])+1)
+
+
+def part1(raw: List[List[str]]):
+    result = 0
+    for line in raw:
+        result += 1 if do_ranges_overlap_completely(line[0], line[1]) else 0
+    return result
+
+
+def part2(raw: List[List[str]]):
+    result = 0
+    for line in raw:
+        result += 1 if do_ranges_overlap(line[0], line[1]) else 0
+    return result
+
+
+def main():
+    raw = read_file_and_split_lines('input.txt', ',')
+    print(f"Part 1: {part1(raw)}")
+    print(f"Part 2: {part2(raw)}")
+
+
+if __name__ == "__main__":
+    main()
--- utils/readers.py
+++ utils/readers.py
@@ -8,3 +8,12 @@ def read_file_as_strings_without_newline(filename: str) -> List[str]:
             temp = line.replace('\n', '')
             out.append(temp)
     return out
+
+
+def read_file_and_split_lines(filename: str, divider: str) -> List[List[str]]:
+    with open(filename, 'r') as file:
+        out = []
+        for line in file:
+            temp = line.replace('\n', '')
+            out.append(temp.split(divider))
+    return out
